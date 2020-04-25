0ac9ba4d15eabf098a829a9880263d65b38960cd
Mond Diolola <28037475+mdiolola@users.noreply.github.com>
2020-04-25 12:14:50 +0800

    initial commit

---
+++ Backend/Controllers/WorkoutsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Data;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutsController : ControllerBase
+    {
+        private readonly WorkoutContext _context;
+
+        public WorkoutsController(WorkoutContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Workouts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Workout>>> GetWorkout()
+        {
+            return await _context.Workout.ToListAsync();
+        }
+
+        // GET: api/Workouts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Workout>> GetWorkout(int id)
+        {
+            var workout = await _context.Workout.FindAsync(id);
+
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            return workout;
+        }
+
+        // PUT: api/Workouts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorkout(int id, Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != workout.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(workout).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkoutExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Workouts
+        [HttpPost]
+        public async Task<ActionResult<Workout>> PostWorkout(Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Workout.Add(workout);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWorkout", new { id = workout.Id }, workout);
+        }
+
+        // DELETE: api/Workouts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Workout>> DeleteWorkout(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var workout = await _context.Workout.FindAsync(id);
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            _context.Workout.Remove(workout);
+            await _context.SaveChangesAsync();
+
+            return workout;
+        }
+
+        private bool WorkoutExists(int id)
+        {
+            return _context.Workout.Any(e => e.Id == id);
+        }
+    }
+}
